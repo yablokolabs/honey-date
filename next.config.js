ad6ce90c59b24fd01c0e649bd93924d8e1567a93
CEO <ceo@map2map.com>
2025-10-18 19:44:54 +0000

    Add GitHub Pages deployment workflow and configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/honey-date',
+  assetPrefix: '/honey-date/',
 }
 
 module.exports = nextConfig
